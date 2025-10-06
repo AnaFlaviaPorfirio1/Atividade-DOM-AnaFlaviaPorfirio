346635cdc24318ff841963d62164338dd97de162
Ana <anafp0518@gmail.com>
2025-10-06 12:43:20 +0000

    ...

---
+++ script.js
@@ -0,0 +1,29 @@
+// Seletores do DOM
+const taskForm = document.getElementById( 'taskForm' );
+const taskImput = document.getElementById( 'taskImput');
+const taskList = document.getElementById( 'taskList');
+
+// função que cria um <li> com texto e botão "Remover"
+function createTaskItem(texto) {
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    span.textContent = text;
+    const deleteBnt = document.createElement('button');
+    deleteBnt.textContent = 'Remover';
+    deleteBnt.className = 'delete-bnt';
+    deleteBnt.type = 'button';
+    li.appendChild(span);
+    li.appendChild(deleteBnt);
+    return li;
+}
+
+// Captura do submit
+taskForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const text = taskImput.value.trim();
+    if (!text) return;
+    const li = createTaskItem(text);
+    taskList.appendChild(li);
+    taskImput.value = '';
+    taskImput.focus();
+});
